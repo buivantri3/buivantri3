daf4a49c244d526a85d2286305893dc779ce0664
tri.bui <tri.bui@tribv.cloud>
2025-11-19 04:42:45 +0000

    update

---
+++ 01-cloud/gcp/landing-zone/terraform/modules/organization-foundation/variables.tf
@@ -0,0 +1,9 @@
+variable "organization_id" {
+  description = "The ID of the GCP organization."
+  type        = string
+}
+
+variable "billing_account" {
+  description = "The ID of the GCP billing account."
+  type        = string
+}
