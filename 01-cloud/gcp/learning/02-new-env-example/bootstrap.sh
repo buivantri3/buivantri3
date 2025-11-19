de560092c7460233fa219b0c662a0a83d3241c7a
tri.bui <tri.bui@tribv.cloud>
2025-11-19 16:43:10 +0000

    update

--- 01-cloud/gcp/learning/02-new-env-example/bootstrap.sh
+++ 01-cloud/gcp/learning/02-new-env-example/bootstrap.sh
@@ -9,3 +9,4 @@ gcloud container clusters get-credentials my-autopilot-cluster --region us-centr
 
 kubectl create deployment hello-server --image=us-docker.pkg.dev/google-samples/containers/gke/hello-app:1.0
 kubectl expose deployment hello-server --type LoadBalancer --port 80 --target-port 8080
+
